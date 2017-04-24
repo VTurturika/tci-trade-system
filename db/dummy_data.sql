4360a742133f9543520c9fb6e12df55fb56503b3
Victor Turturika <v.turturika@gmail.com>
2017-04-24 20:04:15 +0300

    fix(db): improve db schema, fix dummy data script

--- db/dummy_data.sql
+++ db/dummy_data.sql
@@ -1,90 +1,106 @@
-
---
--- Dumping data for table `Category`
---
+-- dummy data for development
 
 LOCK TABLES `Category` WRITE;
-
-INSERT INTO `Category` VALUES (1,'електроніка',0),(2,'господарство',0),(3,'телефони',1),(4,'телевізори',1),(5,'сад і город',2);
-
+INSERT INTO `Category` (name, parent) VALUES
+  ('електроніка', 0),
+  ('господарство', 0),
+  ('телефони', 1),
+  ('телевізори', 1),
+  ('сад і город', 2);
 UNLOCK TABLES;
-
---
--- Dumping data for table `Characteristic`
---
 
 LOCK TABLES `Characteristic` WRITE;
-
-INSERT INTO `Characteristic` VALUES (1,'float','діагональ','дм'),(2,'boolean','тв2 ',''),(3,'integer','кількість ядер','шт'),(4,'float','висота','м,см'),(5,'float','діаметр','м,см');
-
+INSERT INTO `Characteristic` (type, name, measurements) VALUES
+  ('float','діагональ','дм'),
+  ('boolean','тв2 ',''),
+  ('integer','кількість ядер','шт'),
+  ('float','висота','м,см'),
+  ('float','діаметр','м,см');
 UNLOCK TABLES;
-
---
--- Dumping data for table `Characteristic_Category`
---
 
 LOCK TABLES `Characteristic_Category` WRITE;
-
-INSERT INTO `Characteristic_Category` VALUES (1,3),(3,3),(4,3),(1,4),(4,4),(4,2),(5,2);
-
+INSERT INTO `Characteristic_Category`(characteristic, category) VALUES
+  (1,3),
+  (3,3),
+  (4,3),
+  (1,4),
+  (2,4),
+  (4,4),
+  (4,5),
+  (5,5);
 UNLOCK TABLES;
-
---
--- Dumping data for table `Product`
---
 
 LOCK TABLES `Product` WRITE;
-
-INSERT INTO `Product` VALUES (1,'Iphone',NULL,3,'12345','qwerty','23','Apple','7','3','cool',''),(2,'Xiaomi',NULL,3,'5434324','ytrewq','54','MI','Redmi 3','343','Norm',NULL),(3,'Smart TV',NULL,4,'fewrer','56564','4','Samsung','345s','78','fucking cool',NULL),(4,'Regular TV',NULL,4,'567','hthtrhtr','333','China Noname','first','so-so','FX',NULL),(5,'Горщик для квітів',NULL,5,'2324frfr','lflgflg','4','China nigers','3','',NULL,NULL),(6,'Граблі','',5,'455','544','3','Ukraine','nimbulus','2000','qwerty',NULL);
-
+INSERT INTO `Product`(title, description, category, article, barcode, consignment,
+                      manufacturer, model, series, specification, comment) VALUES
+  ('Iphone',NULL,3,'12345','qwerty','23','Apple','7','3','cool',''),
+  ('Xiaomi',NULL,3,'5434324','ytrewq','54','MI','Redmi 3','343','Norm',NULL),
+  ('Smart TV',NULL,4,'fewrer','56564','4','Samsung','345s','78','fucking cool',NULL),
+  ('Regular TV',NULL,4,'567','hthtrhtr','333','China Noname','first','so-so','FX',NULL),
+  ('Горщик для квітів',NULL,5,'2324frfr','lflgflg','4','China nigers','3','',NULL,NULL),
+  ('Граблі','',5,'455','544','3','Ukraine','nimbulus','2000','qwerty',NULL);
 UNLOCK TABLES;
-
---
--- Dumping data for table `Product_Characteristic`
---
 
 LOCK TABLES `Product_Characteristic` WRITE;
-
-INSERT INTO `Product_Characteristic` VALUES (1,1,NULL,NULL,6.999,NULL,'м'),(1,3,NULL,5,NULL,NULL,'м'),(1,4,NULL,NULL,0.25,NULL,'м'),(2,1,NULL,NULL,5,NULL,'м'),(2,3,NULL,8,NULL,NULL,'м'),(2,4,NULL,NULL,0.25,NULL,'м'),(3,1,NULL,NULL,50,NULL,'см'),(3,2,1,NULL,NULL,NULL,'см'),(3,4,NULL,NULL,100,NULL,'см'),(4,1,NULL,NULL,40,NULL,'см'),(4,2,0,NULL,NULL,NULL,'см'),(4,4,NULL,NULL,90,NULL,'см'),(5,4,NULL,NULL,30,NULL,NULL),(5,5,NULL,NULL,20,NULL,NULL),(6,4,NULL,NULL,100,NULL,NULL);
-
+INSERT INTO `Product_Characteristic`(product, characteristic, type, value, measure) VALUES
+  (1,1,'float','6.95','дм'),
+  (1,3,'integer','5','шт'),
+  (1,4,'float','0,25','м'),
+  (2,1,'float','5','дм'),
+  (2,3,'float','8','шт'),
+  (2,4,'float','0.22','м'),
+  (3,1,'float','50','дм'),
+  (3,2,'boolean','true',NULL),
+  (3,4,'float','100','см'),
+  (4,1,'float','40','дм'),
+  (4,2,'boolean','false',NULL),
+  (4,4,'float','90','см'),
+  (5,4,'float','30','см'),
+  (5,5,'float','20','см'),
+  (6,4,'float','100','см');
 UNLOCK TABLES;
-
---
--- Dumping data for table `Counterparty`
---
 
 LOCK TABLES `Counterparty` WRITE;
-
-INSERT INTO `Counterparty` VALUES (1,'Алі Експрес',0,'Тянь','Чан','Шань',NULL,'ali-express@ukr.net','China','хз','хз',NULL),(2,'Барахолка',0,'дядя Вася',NULL,NULL,'102',NULL,'вул. Попова 23',NULL,NULL,NULL);
-
+INSERT INTO `Counterparty` (title,type,firstname,lastname,middlename,
+                            phone,email,address,JP_type,JP_code,comment) VALUES
+  ('Алі Експрес',0,'Тянь','Чан','Шань',NULL,'ali-express@ukr.net','China','хз','хз',NULL),
+  ('Барахолка',1,'дядя Вася',NULL,NULL,'102',NULL,'вул. Попова 23',NULL,NULL,NULL);
 UNLOCK TABLES;
 
---
--- Dumping data for table `Instance`
---
 
 LOCK TABLES `Instance` WRITE;
-
-INSERT INTO `Instance` VALUES (1,4,5,15000.00,1,'2','UAH'),(2,1,10,13000.00,1,'1','UAH'),(3,8,10,3000.00,2,'2','UAH'),(4,3,5,20000.00,3,'2','UAH'),(5,2,10,10000.00,4,'1','UAH'),(6,90,100,35.00,5,'1','UAH'),(7,5,10,100.00,6,'2','UAH');
-
+INSERT INTO `Instance`(product, current_count, buying_count, buying_price, currency, storage) VALUES
+  (1,4,5,15000.00,'UAH','2'),
+  (1,1,10,13000.00,'UAH','1'),
+  (2,8,10,3000.00,'UAH','2'),
+  (3,3,5,20000.00,'UAH','2'),
+  (4,2,10,10000.00,'UAH','1'),
+  (5,90,100,35.00,'UAH','1'),
+  (6,5,10,100.00,'UAH','2');
 UNLOCK TABLES;
-
---
--- Dumping data for table `Transaction`
---
 
 LOCK TABLES `Transaction` WRITE;
-
-INSERT INTO `Transaction` VALUES (1,7,0,439500.00,'накладна',NULL,'2017-04-23'),(2,7,1,267925.00,'чесне слово',NULL,'2017-04-24');
-
+INSERT INTO `Transaction`(total_count, type, total_price,
+                          document, preparing_date, conducted_date) VALUES
+  (7,0,439500.00,'накладна',NULL,'2017-04-23'),
+  (7,1,267925.00,'чесне слово',NULL,'2017-04-24');
 UNLOCK TABLES;
 
---
--- Dumping data for table `Instance_Transaction`
---
-
 LOCK TABLES `Instance_Transaction` WRITE;
-
-INSERT INTO `Instance_Transaction` VALUES (1,1,1,0,0,NULL),(2,1,1,0,NULL,NULL),(3,1,1,0,NULL,NULL),(4,1,1,0,NULL,NULL),(5,1,1,0,NULL,NULL),(6,1,1,0,NULL,NULL),(7,1,1,0,NULL,NULL),(1,2,2,1,1,16000.00),(2,2,2,1,9,12000.00),(3,2,2,1,2,3500.00),(4,2,2,1,2,20000.00),(5,2,2,1,8,12000.00),(6,2,2,1,10,40.00),(7,2,2,1,5,105.00);
-
+INSERT INTO `Instance_Transaction`(instance, transaction, counterparty,
+                                   type, selling_count, selling_price) VALUES
+  (1,1,1,0,NULL,NULL),
+  (2,1,1,0,NULL,NULL),
+  (3,1,1,0,NULL,NULL),
+  (4,1,1,0,NULL,NULL),
+  (5,1,1,0,NULL,NULL),
+  (6,1,1,0,NULL,NULL),
+  (7,1,1,0,NULL,NULL),
+  (1,2,2,1,1,16000.00),
+  (2,2,2,1,9,12000.00),
+  (3,2,2,1,2,3500.00),
+  (4,2,2,1,2,20000.00),
+  (5,2,2,1,8,12000.00),
+  (6,2,2,1,10,40.00),
+  (7,2,2,1,5,105.00);
 UNLOCK TABLES;
--- db/trade_system.sql
+++ db/trade_system.sql
@@ -1,157 +1,162 @@
-# Create schemas
-CREATE SCHEMA IF NOT EXISTS trade_system;
+# Create schema
 
+CREATE SCHEMA IF NOT EXISTS trade_system;
+USE trade_system;
 
 # Create tables
-CREATE TABLE IF NOT EXISTS trade_system.Product_Characteristic
+
+CREATE TABLE IF NOT EXISTS Product
 (
-    product INT NOT NULL,
-    characteristic INT,
-    measure VARCHAR(50),
-    value_bool TINYINT(1),
-    value_int INT,
-    value_double DOUBLE,
-    value_string VARCHAR(50)    
+  id            INT NOT NULL AUTO_INCREMENT,
+  title         VARCHAR(50),
+  description   VARCHAR(500),
+  category      INT,
+  article       VARCHAR(50),
+  barcode       VARCHAR(50),
+  consignment   VARCHAR(50),
+  manufacturer  VARCHAR(100),
+  model         VARCHAR(50),
+  series        VARCHAR(50),
+  specification VARCHAR(50),
+  comment       VARCHAR(256),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS trade_system.Product
+CREATE TABLE IF NOT EXISTS Characteristic
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(50),
-    description VARCHAR(500),
-    category INT,
-    article VARCHAR(50),
-    barcode VARCHAR(50),
-    consignment VARCHAR(50),
-    manufacturer VARCHAR(100),
-    model VARCHAR(50),
-    series VARCHAR(50),
-    specification VARCHAR(50),
-    comment VARCHAR(256),
-    PRIMARY KEY(id)
+  id           INT NOT NULL AUTO_INCREMENT,
+  type         VARCHAR(10),
+  name         VARCHAR(30),
+  measurements VARCHAR(500),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS trade_system.Transaction
+CREATE TABLE IF NOT EXISTS Category
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    total_count INT,
-    type TINYINT(1),
-    total_price DECIMAL(18, 2),
-    document VARCHAR(256),
-    preparing_date DATE,
-    conducted_date DATE,
-    PRIMARY KEY(id)
+  id     INT NOT NULL AUTO_INCREMENT,
+  name   VARCHAR(50),
+  parent INT,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS trade_system.Instance_Transaction
+CREATE TABLE IF NOT EXISTS Characteristic_Category
 (
-    instance INT NOT NULL,
-    transaction INT NOT NULL,
-    counterparty INT,
-    type TINYINT(1),
-    selling_count INT,
-    selling_price DECIMAL(18, 2)    
+  characteristic INT,
+  category       INT
 );
 
-CREATE TABLE IF NOT EXISTS trade_system.Characteristic
+CREATE TABLE IF NOT EXISTS Product_Characteristic
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    type VARCHAR(10),
-    name VARCHAR(30),
-    measurements VARCHAR(500),
-    PRIMARY KEY(id)
+  product        INT NOT NULL,
+  characteristic INT,
+  type           VARCHAR(10),
+  value          VARCHAR(100),
+  measure        VARCHAR(50)
 );
 
-CREATE TABLE IF NOT EXISTS trade_system.Category
+CREATE TABLE IF NOT EXISTS Instance
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50),
-    parent INT,
-    PRIMARY KEY(id)
+  id            INT NOT NULL AUTO_INCREMENT,
+  current_count INT,
+  buying_count  INT,
+  buying_price  DECIMAL(18, 2),
+  currency      VARCHAR(20),
+  product       INT NOT NULL,
+  storage       VARCHAR(20),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS trade_system.Instance
+CREATE TABLE IF NOT EXISTS Transaction
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    current_count INT,
-    buying_count INT,
-    buying_price DECIMAL(18, 2),
-    currency VARCHAR(20),
-    product INT NOT NULL,
-    storage VARCHAR(20),
-    PRIMARY KEY(id)
+  id             INT NOT NULL AUTO_INCREMENT,
+  total_count    INT,
+  type           TINYINT(1),
+  total_price    DECIMAL(18, 2),
+  document       VARCHAR(256),
+  preparing_date DATE,
+  conducted_date DATE,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS trade_system.Counterparty
+CREATE TABLE IF NOT EXISTS Counterparty
 (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(50),
-    type TINYINT(1),
-    firstname VARCHAR(50),
-    lastname VARCHAR(50),
-    middlename VARCHAR(50),
-    phone VARCHAR(20),
-    email VARCHAR(50),
-    address VARCHAR(100),
-    JP_type VARCHAR(50),
-    JP_code VARCHAR(50),
-    comment VARCHAR(256),
-    PRIMARY KEY(id)
+  id         INT NOT NULL AUTO_INCREMENT,
+  title      VARCHAR(50),
+  type       TINYINT(1),
+  firstname  VARCHAR(50),
+  lastname   VARCHAR(50),
+  middlename VARCHAR(50),
+  phone      VARCHAR(20),
+  email      VARCHAR(50),
+  address    VARCHAR(100),
+  JP_type    VARCHAR(50),
+  JP_code    VARCHAR(50),
+  comment    VARCHAR(256),
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS trade_system.Characteristic_Category
+CREATE TABLE IF NOT EXISTS Instance_Transaction
 (
-    characteristic INT,
-    category INT    
+  instance      INT NOT NULL,
+  transaction   INT NOT NULL,
+  counterparty  INT,
+  type          TINYINT(1),
+  selling_count INT,
+  selling_price DECIMAL(18, 2)
 );
-
 
 # Create FKs
-ALTER TABLE trade_system.Product_Characteristic
-    ADD    FOREIGN KEY (product)
-    REFERENCES trade_system.Product(id)
-;
-    
-ALTER TABLE trade_system.Instance_Transaction
-    ADD    FOREIGN KEY (transaction)
-    REFERENCES trade_system.Transaction(id)
-;
-    
-ALTER TABLE trade_system.Product_Characteristic
-    ADD    FOREIGN KEY (characteristic)
-    REFERENCES trade_system.Characteristic(id)
-;
-    
-ALTER TABLE trade_system.Product
-    ADD    FOREIGN KEY (category)
-    REFERENCES trade_system.Category(id)
-;
-    
-ALTER TABLE trade_system.Instance
-    ADD    FOREIGN KEY (product)
-    REFERENCES trade_system.Product(id)
-;
-    
-ALTER TABLE trade_system.Instance_Transaction
-    ADD    FOREIGN KEY (instance)
-    REFERENCES trade_system.Instance(id)
-;
-    
-ALTER TABLE trade_system.Instance_Transaction
-    ADD    FOREIGN KEY (counterparty)
-    REFERENCES trade_system.Counterparty(id)
-;
-    
-ALTER TABLE trade_system.Characteristic_Category
-    ADD    FOREIGN KEY (category)
-    REFERENCES trade_system.Category(id)
-;
-    
-ALTER TABLE trade_system.Characteristic_Category
-    ADD    FOREIGN KEY (characteristic)
-    REFERENCES trade_system.Characteristic(id)
-;
-    
 
-# Create Indexes
+ALTER TABLE Product
+  ADD FOREIGN KEY (category)
+REFERENCES Category (id)
+  ON UPDATE CASCADE
+  ON DELETE SET NULL;
 
+ALTER TABLE Product_Characteristic
+  ADD FOREIGN KEY (product)
+REFERENCES Product (id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+
+ALTER TABLE Product_Characteristic
+  ADD FOREIGN KEY (characteristic)
+REFERENCES Characteristic (id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+
+ALTER TABLE Characteristic_Category
+  ADD FOREIGN KEY (category)
+REFERENCES Category (id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+
+ALTER TABLE Characteristic_Category
+  ADD FOREIGN KEY (characteristic)
+REFERENCES Characteristic (id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+
+ALTER TABLE Instance
+  ADD FOREIGN KEY (product)
+REFERENCES Product (id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+
+ALTER TABLE Instance_Transaction
+  ADD FOREIGN KEY (transaction)
+REFERENCES Transaction (id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+
+ALTER TABLE Instance_Transaction
+  ADD FOREIGN KEY (instance)
+REFERENCES Instance (id)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
+
+ALTER TABLE Instance_Transaction
+  ADD FOREIGN KEY (counterparty)
+REFERENCES Counterparty (id)
+  ON UPDATE CASCADE
+  ON DELETE SET NULL;
