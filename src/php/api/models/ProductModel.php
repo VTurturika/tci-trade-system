7d8e279647d579c23d1286f2370a7a4934d32e91
Victor Turturika <v.turturika@gmail.com>
2017-04-27 04:53:00 +0300

    feat(api): implement /api/product/get/{id}

--- src/php/api/models/ProductModel.php
+++ src/php/api/models/ProductModel.php
@@ -2,27 +2,31 @@
 
 class ProductModel extends Model {
 
+    private $select =
+        "SELECT pc.product, p.title, p.category, p.article, p.barcode, p.consignment, p.manufacturer, "
+        . "p.model, p.series, p.specification, pc.characteristic, c.name, c.type, c.measure, pc.value, "
+        . "it.instance, i.current_count AS count, i.buying_count, i.buying_price, i.currency, "
+        . "t.conducted_date AS date, it.counterparty, ct.title AS counterparty_title "
+        . "FROM Product_Characteristic pc";
+
+    private $characteristicJoin = "JOIN Characteristic c ON c.id = pc.characteristic";
+    private $counterpartyJoin =  "JOIN Counterparty ct ON ct.id = it.counterparty ";
+    private $featuresJoin = "";
+    private $productJoin = "JOIN Product p ON pc.product = p.id ";
+    private $instanceJoin = "JOIN Instance i ON p.id = i.product ";
+    private $instanceTransactionJoin = "JOIN Instance_Transaction it ON i.id = it.instance AND it.type = 0 ";
+    private $transactionJoin = "JOIN Transaction t ON t.id = it.transaction ";
+
     public function get($params, $id) {
 
         if($params == null) $params = array();
         Logger::logWithMsg("params", $params);
 
-        return ($id == null) ? $this->getFilteredProducts($params) : $this->getSpecifiedProduct($params, $id);
+        return ($id == null) ? $this->getFilteredProducts($params) : $this->getSpecifiedProduct($id);
     }
 
     private function getFilteredProducts($params) {
 
-        $mainSelect =
-            "SELECT pc.product, p.title, p.category, p.article, p.barcode, p.consignment, p.manufacturer, "
-            . "p.model, p.series, p.specification, pc.characteristic, c.name, c.type, c.measure, pc.value, "
-            . "it.instance, i.current_count AS count, i.buying_count, i.buying_price, i.currency, "
-            . "t.conducted_date AS date, it.counterparty, ct.title AS counterparty_title "
-            . "FROM Product_Characteristic pc";
-
-        $characteristicJoin = "JOIN Characteristic c ON c.id = pc.characteristic";
-        $counterpartyJoin =  "JOIN Counterparty ct ON ct.id = it.counterparty ";
-
-        $featuresJoin = "";
         if(array_key_exists("characteristics", $params) && count($params["characteristics"]) > 0) {
 
             $i = 1;
@@ -84,67 +88,65 @@ class ProductModel extends Model {
                       "(SELECT product FROM Product_Characteristic WHERE characteristic=$id AND value $valueStr ) f1 "
                     : "JOIN (SELECT product FROM Product_Characteristic " .
                       "WHERE characteristic=$id AND value $valueStr ) f$i ON f1.product = f$i.product ";
-                $featuresJoin .= $featureSelect;
+                $this->featuresJoin .= $featureSelect;
                 $i++;
             }
 
-            if (strpos($featuresJoin, ") f1 ") !== false) {
-                $featuresJoin .= ") products ON pc.product = products.ids";
+            if (strpos($this->featuresJoin, ") f1 ") !== false) {
+                $this->featuresJoin .= ") products ON pc.product = products.ids";
             }
-            else $featuresJoin = "";
+            else $this->featuresJoin = "";
         }
-        //Logger::logWithMsg("featuresJoin", $featuresJoin);
+        //Logger::logWithMsg("featuresJoin", $this->featuresJoin);
 
-        $productJoin = "JOIN Product p ON pc.product = p.id ";
         if(array_key_exists("title", $params)) {
             $title = $params["title"];
-            $productJoin .= "AND p.title = '$title' ";
+            $this->productJoin .= "AND p.title = '$title' ";
         }
         if(array_key_exists("category", $params)) {
             $category = $params["category"];
-            $productJoin .= "AND p.category = $category ";
+            $this->productJoin .= "AND p.category = $category ";
         }
         if(array_key_exists("article", $params)) {
             $article = $params["article"];
-            $productJoin .= "AND p.article = '$article' ";
+            $this->productJoin .= "AND p.article = '$article' ";
         }
         if(array_key_exists("barcode", $params)) {
             $barcode = $params["barcode"];
-            $productJoin .= "AND p.barcode = '$barcode' ";
+            $this->productJoin .= "AND p.barcode = '$barcode' ";
         }
         if(array_key_exists("consignment", $params)) {
             $consignment = $params["consignment"];
-            $productJoin .=  "AND p.consignment = '$consignment' ";
+            $this->productJoin .=  "AND p.consignment = '$consignment' ";
         }
         if(array_key_exists("manufacturer", $params)) {
             $manufacturer = $params["manufacturer"];
-            $productJoin .= "AND p.manufacturer = '$manufacturer' ";
+            $this->productJoin .= "AND p.manufacturer = '$manufacturer' ";
         }
         if(array_key_exists("model", $params)) {
             $model = $params["model"];
-            $productJoin .= "AND p.model = '$model' ";
+            $this->productJoin .= "AND p.model = '$model' ";
         }
         if(array_key_exists("series", $params)) {
             $series = $params["series"];
-            $productJoin .= "AND p.series = '$series' ";
+            $this->productJoin .= "AND p.series = '$series' ";
         }
         if(array_key_exists("specification", $params)) {
             $specification = $params["specification"];
-            $productJoin .= "AND p.specification = '$specification' ";
+            $this->productJoin .= "AND p.specification = '$specification' ";
         }
-        //Logger::logWithMsg("productJoin", $productJoin);
+        //Logger::logWithMsg("productJoin", $this->productJoin);
 
-        $instanceJoin = "JOIN Instance i ON p.id = i.product ";
         if(array_key_exists("price", $params)) {
 
             if($params["price"]["from"] == null && $params["price"]["to"] != null) { //buying_price <= to
-               $instanceJoin .= "AND i.buying_price <= " . floatval($params["price"]["to"]) . " ";
+               $this->instanceJoin .= "AND i.buying_price <= " . floatval($params["price"]["to"]) . " ";
             }
             else if ($params["price"]["from"] != null && $params["price"]["to"] == null) { //buying_price >= from
-                $instanceJoin .= "AND i.buying_price >= " . floatval($params["price"]["from"]) . " ";
+                $this->instanceJoin .= "AND i.buying_price >= " . floatval($params["price"]["from"]) . " ";
             }
             else if ($params["price"]["from"] != null && $params["price"]["to"] != null) {
-                $instanceJoin .= "AND i.buying_price BETWEEN "
+                $this->instanceJoin .= "AND i.buying_price BETWEEN "
                     . floatval($params["price"]["from"]) . " AND "
                     . floatval($params["price"]["to"]) . " ";
             }
@@ -153,64 +155,69 @@ class ProductModel extends Model {
 
 
             if($params["count"]["from"] == null && $params["count"]["to"] != null) { //current_count <= to
-                $instanceJoin .= "AND i.current_count <= " . floatval($params["count"]["to"]) . " ";
+                $this->instanceJoin .= "AND i.current_count <= " . floatval($params["count"]["to"]) . " ";
             }
             else if ($params["count"]["from"] != null && $params["count"]["to"] == null) { //buying_price >= from
-                $instanceJoin .= "AND i.current_count >= " . floatval($params["count"]["from"]) . " ";
+                $this->instanceJoin .= "AND i.current_count >= " . floatval($params["count"]["from"]) . " ";
             }
             else if ($params["count"]["from"] != null && $params["count"]["to"] != null) {
-                $instanceJoin .= "AND i.current_count BETWEEN " . floatval($params["count"]["from"])
-                              . " AND " . floatval($params["count"]["to"]) . " ";
+                $this->instanceJoin .= "AND i.current_count BETWEEN " . floatval($params["count"]["from"])
+                    . " AND " . floatval($params["count"]["to"]) . " ";
             }
         }
         if(array_key_exists("storage", $params)) {
             $storage = $params["storage"];
-            $instanceJoin .= "AND i.storage = '$storage' ";
+            $this->instanceJoin .= "AND i.storage = '$storage' ";
         }
         if(array_key_exists("currency", $params)) {
             $currency = $params["currency"];
-            $instanceJoin .= "AND i.currency = '$currency' ";
+            $this->instanceJoin .= "AND i.currency = '$currency' ";
         }
-        //Logger::logWithMsg("instanceJoin", $instanceJoin);
-
-        $instanceTransactionJoin = "JOIN Instance_Transaction it ON i.id = it.instance AND it.type = 0 ";
+        //Logger::logWithMsg("instanceJoin", $this->instanceJoin);
 
         if(array_key_exists("counterparty", $params)) {
             $counterparty = $params["counterparty"];
-            $instanceTransactionJoin .= "AND it.counterparty = $counterparty ";
+            $this->instanceTransactionJoin .= "AND it.counterparty = $counterparty ";
         }
-        //Logger::logWithMsg("instanceTransactionJoin", $instanceTransactionJoin);
+        //Logger::logWithMsg("instanceTransactionJoin", $this->instanceTransactionJoin);
 
-        $transactionJoin = "JOIN Transaction t ON t.id = it.transaction ";
         if(array_key_exists("data", $params)) {
 
             if($params["data"]["from"] == null && $params["data"]["to"] != null) { //data <= to
                 $to = $params["data"]["to"];
-                $transactionJoin .= "AND t.conducted_date <= '$to' ";
+                $this->transactionJoin .= "AND t.conducted_date <= '$to' ";
             }
             else if ($params["data"]["from"] != null && $params["data"]["to"] == null) { //data >= from
                 $from = $params["data"]["from"];
-                $transactionJoin .= "AND t.conducted_date >= '$from' ";
+                $this->transactionJoin .= "AND t.conducted_date >= '$from' ";
             }
             else if ($params["data"]["from"] != null && $params["data"]["to"] != null) {
                 $from = $params["data"]["from"];
                 $to = $params["data"]["to"];
-                $transactionJoin .= "AND t.conducted_date BETWEEN '$from' AND '$to' ";
+                $this->transactionJoin .= "AND t.conducted_date BETWEEN '$from' AND '$to' ";
             }
         }
-        //Logger::logWithMsg("transactionJoin", $transactionJoin);
+        //Logger::logWithMsg("transactionJoin", $this->transactionJoin);
 
-        $allQuery = "$mainSelect $featuresJoin $characteristicJoin $productJoin $instanceJoin "
-            . "$instanceTransactionJoin $transactionJoin $counterpartyJoin";
+        $fullQuery = "$this->select $this->featuresJoin "
+            . "$this->characteristicJoin $this->productJoin $this->instanceJoin "
+            . "$this->instanceTransactionJoin $this->transactionJoin $this->counterpartyJoin";
 
-        //Logger::logWithMsg("allQuery", $allQuery);
+        //Logger::logWithMsg("$fullQuery", $fullQuery);
 
-        $dbResult = $this->db->query($allQuery)->get();
+        $dbResult = $this->db->query($fullQuery)->get();
         return array_values($this->parseDbProducts($dbResult));
     }
 
-    private function getSpecifiedProduct($params, $id) {
+    private function getSpecifiedProduct($id) {
 
+
+        $fullQuery = "$this->select $this->productJoin AND p.id = $id "
+            . "$this->characteristicJoin $this->instanceJoin "
+            . "$this->instanceTransactionJoin $this->transactionJoin $this->counterpartyJoin";
+
+        $dbResult = $this->db->query($fullQuery)->get();
+        return array_values($this->parseDbProducts($dbResult))[0];
     }
 
     private function parseDbProducts($dbResult) {
