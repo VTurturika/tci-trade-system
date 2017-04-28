0a0efa2f18c0d830338b5df9394ad06ff4329973
Victor Turturika <v.turturika@gmail.com>
2017-04-28 16:45:51 +0300

    feat(api): partially implement /api/transaction/create 2

--- src/php/api/models/TransactionModel.php
+++ src/php/api/models/TransactionModel.php
@@ -21,7 +21,6 @@ class TransactionModel extends Model {
             Logger::logWithMsg("isConducted", $isConducted);
             Logger::logWithMsg("isSelling", $isSelling);
 
-
             $globalData = $this->db->table("Data")->select("*")->get()[0];
             $transaction = array(
                 "type" => $params["type"] == "true" ? 1 : 0,
@@ -30,9 +29,9 @@ class TransactionModel extends Model {
                 "document" => $params["document"],
                 "preparing_date" => $isConducted ? null : $params["date"],
                 "conducted_date" => $isConducted ? $params["date"] : null,
-                "balans_before" =>  floatval($globalData->balans),
-                "balans_after" => 0,
-                "global_index" => $isConducted ? $globalData->transaction_index + 1 : null
+                "balance_before" =>  floatval($globalData->balance),
+                "balance_after" => 0,
+                "index" => $isConducted ? $globalData->transaction_index + 1 : null
             );
             $transactionId = $this->db->table("Transaction")->insert($transaction);
 
@@ -42,15 +41,15 @@ class TransactionModel extends Model {
                     ->select("*")
                     ->where("product", "=", $i["product"])
                     ->where("buying_price", "=", $i["price"])
-                    ->where("buying_count", "=", $i["count"])
                     ->get();
 
-                if(count($instance) == 0 && !$isSelling) {
+                if(count($instance) == 0) {
 
                     $instance = array(
                         "product" => $i["product"],
                         "buying_price" => $i["price"],
                         "buying_count" => $i["count"],
+                        "current_count" => $i["count"],
                         "currency" => $i["currency"],
                         "storage" => $i["storage"]
                     );
@@ -58,6 +57,7 @@ class TransactionModel extends Model {
                     $instanceId = $instance["id"];
                 }
                 else {
+                    Logger::log($instance);
                     $instance = $instance[0];
                     $instanceId = $instance->id;
                 }
@@ -68,20 +68,26 @@ class TransactionModel extends Model {
                     "counterparty" => $params["counterparty"],
                     "type" => $isSelling ? 1 : 0,
                     "selling_price" =>  $isSelling ? $i["price"] : null,
-                    "selling_count" => $isSelling ? $i["sel"] : null
+                    "selling_count" => $isSelling ? $i["count"] : null
                 );
-
+                $count = $i["count"];
                 $this->db->table("Instance_Transaction")->insert($instanceTransaction);
+                if($isSelling) {
+                    //todo add check negative current_count
+                    $this->db->query("UPDATE Instance SET current_count = current_count - $count " .
+                                     "WHERE id=$instanceId;");
+                }
                 $transaction["total_count"]++;
                 $transaction["total_price"] += $i["count"] * $i["price"];
             }
 
             //todo check negative balance
-            $transaction["balans_after"] = $globalData->balans - $transaction["total_price"];
+            $transaction["balance_after"] = $globalData->balance +
+                $isSelling ? +$transaction["total_price"] : -$transaction["total_price"];
 
-            $this->db->table("Transaction")->update($transaction)->where("id", "=", $transactionId);
+            $this->db->table("Transaction")->where("id", "=", $transactionId)->update($transaction);
             $this->db->table("Data")->update(array(
-                "balans" => $transaction["balans_after"],
+                "balance" => $transaction["balance_after"],
                 "transaction_index" => $isConducted
                     ? $globalData->transaction_index + 1
                     : $globalData->transaction_index
